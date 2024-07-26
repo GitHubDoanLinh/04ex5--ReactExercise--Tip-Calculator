f32b5cfc705d7404a210b400c23a24fd1231190d
Doan Huu Linh <doanlinh1510@gmail.com>
2024-07-26 17:36:45 +0700

    commit 1 build UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TipCalculator></TipCalculator>
     </div>
   );
 }
-
-export default App;
+function TipCalculator() {
+  return(
+    <div>
+    <BillInput></BillInput>
+    <SelectedPercentage>How did you like the service?</SelectedPercentage>
+    <SelectedPercentage>How did your friend like the service?</SelectedPercentage>
+    <Output/>
+    <Reset/>
+  </div>
+  )
+}
+function BillInput() {
+  return(
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" placeholder="bill value"/>
+    </div>
+  )
+}
+function SelectedPercentage({children}) {
+  return(
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value={"0"}>Dissatisfied (0%)</option>
+        <option value={"5"}>It was OK (5%)</option>
+        <option value={"10"}>It was GOOD (10%)</option>
+        <option value={"20"}>Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  )
+}
+function Output(){
+  return(
+    <h3>You pay X ($A + $B tip)</h3>
+  )
+}
+function Reset(){
+  return(
+    <button>Reset</button>
+  )
+}
