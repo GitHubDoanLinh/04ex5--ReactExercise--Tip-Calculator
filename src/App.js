e1e6e5f5386be2927b1299044fab7394920a0a90
Doan Huu Linh <doanlinh1510@gmail.com>
2024-07-27 14:24:52 +0700

    commit02 add Stage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./style.css"
+import "./style.css";
 import { useState } from "react";
 
 export default function App() {
@@ -9,44 +9,58 @@ export default function App() {
   );
 }
 function TipCalculator() {
-  return(
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+  return (
     <div>
-    <BillInput></BillInput>
-    <SelectedPercentage>How did you like the service?</SelectedPercentage>
-    <SelectedPercentage>How did your friend like the service?</SelectedPercentage>
-    <Output/>
-    <Reset/>
-  </div>
-  )
+      <BillInput bill={bill} setBill={setBill}></BillInput>
+      <SelectedPercentage
+        percentage={percentage1}
+        setPercentage={setPercentage1}
+      >
+        How did you like the service?
+      </SelectedPercentage>
+      <SelectedPercentage
+        percentage={percentage2}
+        setPercentage={setPercentage2}
+      >
+        How did your friend like the service?
+      </SelectedPercentage>
+      <Output bill={bill} />
+      <Reset />
+    </div>
+  );
 }
-function BillInput() {
-  return(
+function BillInput({ bill, setBill }) {
+  return (
     <div>
       <label>How much was the bill?</label>
-      <input type="text" placeholder="bill value"/>
+      <input
+        type="text"
+        placeholder="bill value"
+        value={bill}
+        onChange={(e) => Number(setBill(e.target.value))}
+      />
     </div>
-  )
+  );
 }
-function SelectedPercentage({children}) {
-  return(
+function SelectedPercentage({ percentage, setPercentage, children }) {
+  return (
     <div>
       <label>{children}</label>
-      <select>
+      <select value={percentage} onChange={(e) => Number(e.target.value)}>
         <option value={"0"}>Dissatisfied (0%)</option>
         <option value={"5"}>It was OK (5%)</option>
         <option value={"10"}>It was GOOD (10%)</option>
         <option value={"20"}>Absolutely amazing! (20%)</option>
       </select>
     </div>
-  )
+  );
 }
-function Output(){
-  return(
-    <h3>You pay X ($A + $B tip)</h3>
-  )
+function Output({ bill }) {
+  return <h3>You pay X ({bill} + $B tip)</h3>;
 }
-function Reset(){
-  return(
-    <button>Reset</button>
-  )
+function Reset() {
+  return <button>Reset</button>;
 }
